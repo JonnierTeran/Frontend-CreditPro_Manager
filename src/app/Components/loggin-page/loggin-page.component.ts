15cc5442ec41cceaa4c345f2fef28fb36cec85bb
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-20 15:04:37 -0500

    Maquetado de componente loggin

---
+++ src/app/Components/loggin-page/loggin-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loggin-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './loggin-page.component.html',
+  styleUrl: './loggin-page.component.css'
+})
+export class LogginPageComponent {
+
+}
