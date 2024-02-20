7d0adb8fdb48aa0db718930e76f149cedcb39c52
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-20 16:05:15 -0500

    Maquetado SingUp Page

--- src/app/Components/loggin-page/loggin-page.component.ts
+++ src/app/Components/loggin-page/loggin-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-loggin-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './loggin-page.component.html',
   styleUrl: './loggin-page.component.css'
 })
--- src/app/Components/sing-up-page/sing-up-page.component.ts
+++ src/app/Components/sing-up-page/sing-up-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sing-up-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sing-up-page.component.html',
   styleUrl: './sing-up-page.component.css'
 })
