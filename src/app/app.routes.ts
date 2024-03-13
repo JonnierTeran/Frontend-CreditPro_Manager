c295cd3585384446edb93ce4ecb49bd535d5995f
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-03-13 15:37:31 -0500

    Feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LogginPageComponent } from './Components/loggin-page/loggin-page.compon
 import { ErrorPageComponent } from './Components/error-page/error-page.component';
 import { SingUpPageComponent } from './Components/sing-up-page/sing-up-page.component';
 
+
+//Se exporta Arrays con Objetos que contienen las rutas y componentes
 export const routes: Routes = [
-    { path: "", component: LogginPageComponent, title: "Loggin Page" },
-    { path: "loggin", component: LogginPageComponent, title: "Loggin Page" },
-    {path: "singUp" , component: SingUpPageComponent, title: "Registro Admin page"},
+    { path: "", component: LogginPageComponent, title: "Loggin Page" }, //Pagina defoult
+    { path: "loggin", component: LogginPageComponent, title: "Loggin Page" }, // pagina de loggin
+    {path: "singUp" , component: SingUpPageComponent, title: "Registro Admin page"}, //pagina de singUp
     {path: '404', component: ErrorPageComponent }, // Pagina de not found
     { path: '**', redirectTo: '/404' } // Redirige cualquier ruta no encontrada a la página de error
 ];
